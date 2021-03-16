0acb8edf2fed5d569ba40dfec4e2b3832034a1b2
SAKSHI GUPTA <sakshicls11@gmail.com>
2021-03-16 18:53:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app=document.getElementById('app');
-var t=0;
+var total_cost=0;
 async function fetchdata(){
      await fetch('./data.json')
          .then((response)=> response.json())
@@ -11,11 +11,13 @@ async function fetchdata(){
 fetchdata();
 
 
+
+
 function showContent(data){
 var x;
 data.forEach(post =>{  
  
- q=post.quantity;
+ item_quantity=post.quantity;
 
  app.innerHTML+=`
  <div class ="items">
@@ -25,9 +27,9 @@ data.forEach(post =>{
          <td> ${post.name} </td>
          <td> ${post.price} </td>
          <td>
-         <button id="decrease-button" onclick="decrease(${post.id},${post.price})">-</button>
-         <input id=${post.id} type="text" size="1" min="1" value="${q}">
-         <button id='increase-button' onclick="increase(${post.id},${post.price})">+</button>     
+         <button id="decrease-button" onclick="decrease_quantity(${post.id},${post.price})">-</button>
+         <input id=${post.id} type="text" size="1" min="1" value="${item_quantity}">
+         <button id='increase-button' onclick="increase_quantity(${post.id},${post.price})">+</button>     
         
         </td>
          <td>
@@ -37,44 +39,57 @@ data.forEach(post =>{
  </table>
 <hr>
 </div>`
-t=t+parseInt(post.price);
-total(t);
+total_cost=total_cost+parseInt(post.price);
+total(total_cost);
 });
 
 }
 
-function increase(item,p)
+
+
+function increase_quantity(item,price)
 {
 
-y="t"+item;
+id="t"+item;
 document.getElementById(item).value=parseInt(document.getElementById(item).value)+1;
-c=p*parseInt(document.getElementById(item).value);
-document.getElementById(y).innerHTML=c;
-console.log(document.getElementById(y).innerHTML);
-t=t+parseInt(p);
-total(t);
+
+change_cost(id,item,price);
+
+total_cost=total_cost+parseInt(price);
+total(total_cost);
 }
 
-function decrease(item,p)
+
+
+function decrease_quantity(item,price)
 {
-// console.log(d)
-y="t"+item;
+
+id="t"+item;
 quantity=parseInt(document.getElementById(item).value)-1;
 if (quantity<0)
 {
     quantity=0;
 }
 document.getElementById(item).value=quantity;
-c=p*parseInt(document.getElementById(item).value);
-document.getElementById(y).innerHTML=c;
-console.log(document.getElementById(y).innerHTML);
- t=t-parseInt(p);
- total(t);
+change_cost(id,item,price);
+
+ total_cost=total_cost-parseInt(price);
+ total(total_cost);
 }
+
+
+
+function change_cost(id,item,price)
+{
+    cost=price*parseInt(document.getElementById(item).value);
+    document.getElementById(id).innerHTML=cost;
+}
+
+
 
 function total(price)
 {
   
   document.getElementById("subtotal").innerHTML="Subtotal price :  Rs.  "+ price;
- document.getElementById("tot").innerHTML="Total price :  Rs.  "+ price;
+ document.getElementById("total").innerHTML="Total price :  Rs.  "+ price;
 }
